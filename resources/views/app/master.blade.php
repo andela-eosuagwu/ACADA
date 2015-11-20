7a27d52bb48b99b2041ed2696d316ac704d228b0
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-20 15:48:06 +0100

    add secure_asset

--- resources/views/app/master.blade.php
+++ resources/views/app/master.blade.php
@@ -5,20 +5,31 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <link href="{{ asset('res/css/bootstrap.css')  }}" rel='stylesheet' type='text/css' />        
+        <link href="{{ secure_asset('res/css/bootstrap.css')  }}" rel='stylesheet' type='text/css' />        
+        
         <link href="{{ asset('res/css/style.css')  }}" rel='stylesheet' type='text/css' />
+        <link href="{{ secure_asset('res/css/style.css')  }}" rel='stylesheet' type='text/css' />
+        
         <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
         
         <!-- custom -->
         <link href="{{ asset('res/css/custom.css')  }}" rel='stylesheet' type='text/css' />
+        <link href="{{ secure_asset('res/css/custom.css')  }}" rel='stylesheet' type='text/css' />
 
         <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
         <script type="text/javascript" src="{{ asset('res/js/jquery-1.11.1.min.js')  }}"></script>
+        <script type="text/javascript" src="{{ secure_asset('res/js/jquery-1.11.1.min.js')  }}"></script>
+        
         <script type="text/javascript" src="{{ asset('res/js/login.js')  }}"></script>
+        <script type="text/javascript" src="{{ secure_asset('res/js/login.js')  }}"></script>
         <script src="{{ asset('res/js/jquery.easydropdown.js')  }}"></script>
         
         <!--Animation-->
         <script src="{{ asset('res/js/wow.min.js')  }}"></script>
+        <script src="{{ secure_asset('res/js/wow.min.js')  }}"></script>
+        
         <link href="{{ asset('res/css/animate.css')  }}" rel='stylesheet' type='text/css' />
+        <link href="{{ secure_asset('res/css/animate.css')  }}" rel='stylesheet' type='text/css' />
         <script> new WOW().init();</script>
     </head>
     <body>
@@ -41,8 +52,13 @@
         @include('app.includes.sections.footer')
         
         <script src="{{ asset('res/js/classie.js')  }}"></script>
+        <script src="{{ secure_asset('res/js/classie.js')  }}"></script>
+        
         <script src="{{ asset('res/js/uisearch.js')  }}"></script>
+        <script src="{{ secure_asset('res/js/uisearch.js')  }}"></script>
+        
         <script> new UISearch( document.getElementById( 'sb-search' ) ); </script>
         <script type="text/javascript" src="{{ asset('res/js/responsive-nav.js') }}"></script>
+        <script type="text/javascript" src="{{ secure_asset('res/js/responsive-nav.js') }}"></script>
     </body>
 </html>
