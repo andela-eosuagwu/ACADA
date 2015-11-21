537133a4ffe7b5f39d989c9d14f5abb857579fcd
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-21 23:57:55 +0100

    add test case

---
+++ tests/LoginPageTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\User as User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginPageTest extends TestCase
+{
+	
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+	/**
+	 * Test if a user can log in to the system
+	 *
+	 * This test presumes that the user exists
+	 */
+	// public function testLoginFormWorksCorrectly()
+	// {
+	// 	//create a user
+	// 	$this->createUser();
+
+	// 	$this->visit('/login')
+	// 		->type('john@doe.com','email')
+	// 		->type('password', 'password')
+	// 		->press('submit')
+	// 		->seePageIs('/');
+	// }
+
+
+	// public function createUser()
+	// {
+	// 	User::create([
+	// 	  'name' => 'johndoe',
+	// 	  'email' => 'john@doe.com',
+	// 	  'password'=> bcrypt('password')
+	// 	]);
+	// }
+}
