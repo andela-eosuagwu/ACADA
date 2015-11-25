9eb87b9d043ab8e5cfd1ea75a84b5cfcc6793d1e
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-25 18:39:47 +0100

    update nav bar

--- resources/views/app/includes/sections/top_nav.blade.php
+++ resources/views/app/includes/sections/top_nav.blade.php
@@ -48,7 +48,10 @@
                 @if( is_null(Auth::user()->avater) || Auth::user()->avater == '' )
                     <a href="/user/{{Auth::user()->id}}" id=""><img style = "width:40px; margin-top:-14px;" src="{{ asset('res/images/profile.png') }}"><span>{{Auth::user()->username}}</span></a>     
                 @endif
-                <a href="/user/{{Auth::user()->id}}" id=""><img style = "width:50px; margin-top:-10px;"src="{{ Auth::user()->avater }}"><span>{{Auth::user()->username}}</span></a>     
+                @if( ! is_null(Auth::user()->avater) )
+                    <a href="/user/{{Auth::user()->id}}" id=""><img style = "width:50px; margin-top:-10px;"src="{{ Auth::user()->avater }}"><span>{{Auth::user()->username}}</span></a>     
+                @endif
+
                 <a style ="margin-left:40px;" href="/logout" id=""><span>logout</span></a>     
             @endif
         </div>
