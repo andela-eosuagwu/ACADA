051985e54b96182be65e9a5523e1270c9dbaeee5
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-11-29 17:59:01 +0100

    update test case

--- resources/views/app/includes/contents/signup.blade.php
+++ resources/views/app/includes/contents/signup.blade.php
@@ -50,34 +50,6 @@
                 </center>
             </form>
             
-
-
-<!--             <form action="signup" method="get">
-                <input type="" name="_token" value="{!! csrf_token() !!}">
-                <fieldset>
-
-                    <div class="form-group">
-                        <label for="disabledTextInput">username</label>
-                        <input type="text" name="username" id="disabledTextInput" class="form-control" placeholder="username">
-                    </div>
-                   
-                    <div class="form-group">
-                        <label for="disabledTextInput">email</label>
-                        <input type="email" name="email" id="disabledTextInput" class="form-control" placeholder="email">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="disabledTextInput">password</label>
-                        <input type="password" name="password" id="disabledTextInput" class="form-control" placeholder="password">
-                    </div>
-                
-                    <button type="submit" class="btn btn-primary">Register</button>
-                    <a href="/login/facebook" class="btn btn-primary">Facebook</a>
-                    <a href="/login/twitter" class="btn btn-primary">Twitter</a>
-                    <a href="/login/github" class="btn btn-primary">Github</a>
-
-                </fieldset>
-            </form> -->
         </div>
     </div>
 </div>
