3424963171f330618cb5f9aabe13bb43531b7c36
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-12-08 19:04:12 +0100

    fix occupation bug

--- resources/views/app/pages/index.blade.php
+++ resources/views/app/pages/index.blade.php
@@ -11,7 +11,6 @@
 	| @location includes/sections/banner.blade.php
 	 -->
     @include('app.includes.sections.banner')
-    @include('app.includes.contents.create')
 
 	<!-- 
 	| include main content to home page
