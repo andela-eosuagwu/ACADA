3765ce049f5d87e1685197d9a82e2e21dcb8b70f
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-12-09 20:25:25 +0100

    player

--- resources/views/app/includes/sections/player.blade.php
+++ resources/views/app/includes/sections/player.blade.php
@@ -13,7 +13,7 @@
 	    <div class="container_wrap margin-1">
 	        <h1>{{$video->title}}</h1>
 	        <h1 style="font-size:12px; margin-top: -20px; margin-bottm: 50px;">{{$video->description}}</h1>
-	        <iframe style=" margin-top: 10px;" class="player" src="{{$video->src}}" frameborder="0" allowfullscreen></iframe>
+	        <iframe style=" margin-top: 10px;" class="player" src="https://youtube.com/embed/{{ $video->src }}" frameborder="0" allowfullscreen></iframe>
 	    </div>
 	</div>	
 @endforeach
