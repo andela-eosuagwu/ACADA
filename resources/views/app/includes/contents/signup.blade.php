6174c06e00669be394d01eef043081163f9abe06
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2016-01-19 11:02:35 +0100

    Update signup.blade.php

--- resources/views/app/includes/contents/signup.blade.php
+++ resources/views/app/includes/contents/signup.blade.php
@@ -40,7 +40,7 @@
                 </div>
 
                 <div class="form-group">
-                    <button class="form-control btn btn-primary" type="submit">Login</button>
+                    <button class="form-control btn btn-primary" type="submit">Sign Up</button>
                 </div>
                 <center>
                     
